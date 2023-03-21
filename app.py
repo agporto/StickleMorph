e97b48445e3de81576bc1702934f28295b08ae05
Arthur Porto <agporto@gmail.com>
2023-03-21 00:10:35 -0700

    init dev

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 import cv2
 import dlib
 import glob
@@ -7,6 +8,7 @@ import PIL
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from streamlit_javascript import st_javascript
+import time
 
 
 
@@ -17,6 +19,13 @@ def clear_cache():
     st.session_state.landmarks = False
     st.session_state.initial = []
 
+def reorder_landmarks(landmarks):
+    parts = map(str, range(0, len(landmarks)))
+    ids = sorted(list(parts))
+    num = [int(i) for i in ids]
+    new_landmarks = [x for _, x in sorted(zip(num, landmarks))]
+    return new_landmarks
+
 def format_labels(predictor):
     """Format predictor labels."""
     return predictor.split("/")[-1].split(".")[0]
@@ -26,14 +35,11 @@ def download_csv(session):
     if session['landmarks'] is False:
         session['landmarks'] = session['initial']
     df = pd.DataFrame(session['landmarks'], columns=['x', 'y'])
-    return df.to_csv(index=False).encode("utf-8")
+    return df.to_csv(index=True).encode("utf-8")
 
-def landmarks_to_fabric_circles(landmarks, point_color, text_color):
+def landmarks_to_fabric(landmarks, point_color, text_color):
     """Convert landmarks to fabric circles."""
     circles_and_numbers = []
-    parts = map(str, range(0, len(landmarks)))
-    ids = sorted(list(parts))
-
     for i, (x, y) in enumerate(landmarks):
         circles_and_numbers.append({
             "type": "circle",
@@ -45,7 +51,7 @@ def landmarks_to_fabric_circles(landmarks, point_color, text_color):
         })
         circles_and_numbers.append({
             "type": "text",
-            "text": str(ids[i]),
+            "text": str(i),
             "left": x + 10,
             "top": y + 10,
             "fontSize": 16,
@@ -90,8 +96,6 @@ cola,colb = st.sidebar.columns(2)
 landmark_color = cola.color_picker("Landmark: ", "#00ff00")
 stroke_color = colb.color_picker("Text: ", "#ffffff")
 
-
-
 # Main area
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"], key="image", on_change = clear_cache)
 
@@ -110,6 +114,7 @@ if uploaded_file is not None:
     predictor = dlib.shape_predictor(selected_model)
     shape = predictor(image,rect)
     landmarks = [(point.x, point.y) for point in shape.parts()]
+    landmarks = reorder_landmarks(landmarks)
     st.session_state.initial = landmarks.copy()
 
     # Create a canvas to draw and update landmarks
@@ -118,12 +123,11 @@ if uploaded_file is not None:
             st.session_state.landmarks = st.session_state.initial
         if st.session_state.landmarks is not False:
             landmarks = st.session_state.landmarks
-        parts = map(str, range(0, len(landmarks)))
-        ids = sorted(list(parts))
+
         deviation = int(10*(1/ratio))
         for i, (x, y) in enumerate(landmarks):
             image = cv2.circle(image, (x, y), int(7/ratio), PIL.ImageColor.getcolor(landmark_color,'RGB'), -1)
-            image = cv2.putText(image, str((ids[i])), (x+deviation, y+deviation*2), cv2.FONT_HERSHEY_SIMPLEX, 0.5/ratio, PIL.ImageColor.getcolor(stroke_color,'RGB'), int(0.8/ratio), cv2.LINE_AA)
+            image = cv2.putText(image, str(i), (x+deviation, y+deviation*2), cv2.FONT_HERSHEY_SIMPLEX, 0.5/ratio, PIL.ImageColor.getcolor(stroke_color,'RGB'), int(0.8/ratio), cv2.LINE_AA)
         st.image(image, width=maximum, clamp=True)
         if clear:
             st.session_state.landmarks = st.session_state.initial
@@ -147,11 +151,11 @@ if uploaded_file is not None:
             height=image.shape[0],
             width=image.shape[1],
             drawing_mode=['transform'],
-            initial_drawing=landmarks_to_fabric_circles(landmarks, landmark_color, stroke_color),
+            initial_drawing=landmarks_to_fabric(landmarks, landmark_color, stroke_color),
             key="canvas",
         )
 
-
+        time.sleep(1)
         objects = pd.json_normalize(canvas_result.json_data["objects"])
 
         circles = objects[objects['type'] == 'circle']
@@ -164,7 +168,6 @@ if uploaded_file is not None:
         final_df = circles[['landmark_id', 'left', 'top']]
         final_df.columns = ['landmark_id', 'x', 'y']
         final_df.landmark_id = final_df.landmark_id.astype(int)
-        #final_df = final_df.sort_values(by='landmark_id').reset_index(drop=True)
 
         if submit:
             final_df['x'] = final_df['x'] + 5
@@ -179,7 +182,6 @@ if uploaded_file is not None:
                 file_name="landmarks.tps",
                 mime="text/tps",
                 use_container_width=True,
-                #on_click= download_tps,
     )
     cold.download_button(
                 label="Download CSV",
@@ -187,7 +189,6 @@ if uploaded_file is not None:
                 file_name="landmarks.csv",
                 mime="text/csv",
                 use_container_width=True,
-                #on_click= download_csv,
     )
 
 
